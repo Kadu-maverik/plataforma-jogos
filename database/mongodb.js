fdb6150c9aa8fab8a88c8336773231ae1c6a1e45
Kadu-maverik <chihunzinh0@gmail.com>
2026-06-25 17:42:08 +0300

    fix mongo connection

--- database/mongodb.js
+++ database/mongodb.js
@@ -5,9 +5,11 @@ const conectarMongo = async ()=>{
 
     try{
 
+        
+
         await mongoose.connect(
-            process.env.MONGO_URL
-        );
+    process.env.MONGO_URI
+);
 
 
         console.log(
